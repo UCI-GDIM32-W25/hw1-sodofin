37df18af56ca9c2a8daee0339ca72eb6f660f74a
sodofin <sodofin@uci.edu>
2025-01-09 13:34:00 -0800

    Movement done

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,12 +13,27 @@ public class Player : MonoBehaviour
 
     private void Start ()
     {
-        
+        _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
     }
 
     private void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            _playerTransform.Translate(Vector3.up * _speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            _playerTransform.Translate(Vector3.down * _speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            _playerTransform.Translate(Vector3.right * _speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            _playerTransform.Translate(Vector3.left * _speed * Time.deltaTime);
+        }
     }
 
     public void PlantSeed ()
